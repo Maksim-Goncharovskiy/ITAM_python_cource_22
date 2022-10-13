748ac715f6fb7c8bc58c0a02c49de94fb10cb405
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-10-13 20:03:23 +0300

    Update 1-B.py

--- homework/chapter-2/1-B.py
+++ homework/chapter-2/1-B.py
@@ -1,1 +1,8 @@
-
+def summation(a, b):
+    start = min(a, b)
+    end = max(a, b)
+    S = 0 #Сумма
+    for i in range(start, end + 1):
+        S += i
+    return S
+print(summation(int(input()), int(input())))
