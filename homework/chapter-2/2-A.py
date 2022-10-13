a42b530a4902b39fd2e477d21d4bdff16cfcc7fc
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-10-13 20:34:07 +0300

    Update 2-A.py

--- homework/chapter-2/2-A.py
+++ homework/chapter-2/2-A.py
@@ -1,1 +1,8 @@
-
+n = int(input())
+cnt = 1 #количество блоков, необходимых для выстраивания следующего уровня
+ans = 0
+while n >= cnt:
+    ans += 1
+    n = n - cnt
+    cnt = cnt + 1
+print(ans)
